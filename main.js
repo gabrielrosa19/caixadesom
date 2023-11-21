e49b6f83099139dff10239792d0f0f37b5fb463d
Gabriel15 <105285083+gabrielrosa19@users.noreply.github.com>
2023-11-21 09:17:06 -0300

    onkeyup

--- main.js
+++ main.js
@@ -19,6 +19,9 @@ for(let contador = 0;contador < listaDeTeclas.length;contador++){
         tocaSom(idAudio);
     }
     tecla.onkeydown = function(){
-        tecla.classList.add('ativa')
-    }  
+        tecla.classList.add('ativa');
+    } 
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
